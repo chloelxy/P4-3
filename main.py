22c7a64c0918f485f2cf657fe4e69e2e19aa2dc9
chloelxy <chloelxy0911@gmail.com>
2025-09-23 17:06:23 +0800

    second version

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-import yfinance as yf
-import matplotlib.pyplot as plt
-import pandas as pd
-import os
-from datetime import datetime
-
-# load the dataset
-def dataset(stock, period):
-    stock = yf.Ticker(stock)
-    hist = stock.history(period=period)
-    df = pd.DataFrame(hist)
-    print("Your dataframe is:\n")
-    print(df)
-    return df
-
-
-
 # front end codes: graph 
 
 # main to run the program, to add each persons functions together
